0ba1dcbbbd40c0e102c43c202833d92d9a5e587a
elipmoc <relipmoc101@gmail.com>
2019-07-09 05:41:22 +0900

    [feat]構造のデータ構造定義

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod ty;
 mod id;
+mod syntax;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/syntax.rs
@@ -0,0 +1,40 @@
+use crate::id::VarId;
+use crate::ty::Ty;
+
+type BSyntax = Box<Syntax>;
+
+//構文の構造定義
+pub enum Syntax {
+    Unit,
+    Bool(bool),
+    Int(i32),
+    Float(f32),
+    Not(BSyntax),
+    Neg(BSyntax),
+    Add(BSyntax, BSyntax),
+    Sub(BSyntax, BSyntax),
+    FNeg(BSyntax),
+    FAdd(BSyntax, BSyntax),
+    FSub(BSyntax, BSyntax),
+    FMul(BSyntax, BSyntax),
+    FDiv(BSyntax, BSyntax),
+    Eq(BSyntax, BSyntax),
+    LE(BSyntax, BSyntax),
+    If(BSyntax, BSyntax, BSyntax),
+    Let((VarId, Ty), BSyntax, BSyntax),
+    Var(VarId),
+    LetRec(Box<FunDef>, BSyntax),
+    App(BSyntax, Vec<BSyntax>),
+    Tuple(Vec<BSyntax>),
+    LetTuple((VarId, Ty), Vec<BSyntax>),
+    Array(BSyntax, BSyntax),
+    Get(BSyntax, BSyntax),
+    Put(BSyntax, BSyntax, BSyntax),
+}
+
+//関数定義
+pub struct FunDef {
+    name: (VarId, Ty),
+    args: Vec<(VarId, Ty)>,
+    body: Syntax,
+}
