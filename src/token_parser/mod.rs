76943d0df800c625f658d9a1c610f0b608decf5e
elipmoc <relipmoc101@gmail.com>
2019-07-13 21:40:38 +0900

    [feat]演算子のパーサ追加

--- src/token_parser/mod.rs
+++ src/token_parser/mod.rs
@@ -1,3 +1,12 @@
+// 特定の文字列にマッチしてunitを返すパーサを簡易定義できるマクロ
+macro_rules! define_ignore_str_parser {
+    ($f_name:ident,$s:expr) => {
+        named!(pub $f_name<()>,do_parse!(
+            tag!($s)>>(())
+        ));
+    };
+}
+
 mod comment;
 mod number;
 mod bool;
--- src/token_parser/op.rs
+++ src/token_parser/op.rs
@@ -1,7 +1,22 @@
 /// not演算子のパーサ
-named!(pub not_parser<()>,do_parse!(
-    tag!("not")>>(())
-));
+define_ignore_str_parser!(not_parser,"not");
+
+/// -演算子のパーサ
+define_ignore_str_parser!(minus_parser,"-");
+
+/// +演算子のパーサ
+define_ignore_str_parser!(plus_parser,"+");
+
+// その他よくわからんパーサ
+define_ignore_str_parser!(plus_dot_parser,"+.");
+define_ignore_str_parser!(ast_dot_parser,"*.");
+define_ignore_str_parser!(slash_dot_parser,"/.");
+define_ignore_str_parser!(equal_parser,"=");
+define_ignore_str_parser!(less_greater_parser,"<>");
+define_ignore_str_parser!(less_equal_parser,"<=");
+define_ignore_str_parser!(greater_equal_parser,">=");
+define_ignore_str_parser!(less_parser,"<");
+define_ignore_str_parser!(greater_parser,">");
 
 #[test]
 fn not_token_parser_test() {
