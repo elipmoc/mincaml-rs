7ac5a0e6c9cc6f18bd8423cd52d610d6a3d387f8
elipmoc <relipmoc101@gmail.com>
2019-07-13 17:03:51 +0900

    [feat]コメント文のパーサ追加

--- src/token_parser.rs
+++ src/token_parser.rs
@@ -1,5 +1,25 @@
 use crate::util::*;
 
+named!(comment_begin_parser<()>,do_parse!(
+    tag!("(*")>>(())
+));
+
+named!(comment_end_parser<()>,do_parse!(
+    tag!("*)")>>(())
+));
+
+named!(comment_parser<()>,do_parse!(
+    comment_begin_parser
+    >> many0!(
+        alt!(
+           comment_parser |
+           map!(preceded!(not!(comment_end_parser),take!(1)),|_|(()))
+        )
+       )
+    >> comment_end_parser
+    >> (())
+));
+
 named!(lparen_parser<()>,do_parse!(
     tag!("(") >> (())
 ));
@@ -23,16 +43,16 @@ named!(not_parser<()>,do_parse!(
 #[test]
 fn bool_token_parser_test() {
     let result = bool_parser("false".as_bytes());
-    assert_ok!(result,false);
+    assert_full_match_ok!(result,false);
 
     let result = bool_parser("true".as_bytes());
-    assert_ok!(result,true);
+    assert_full_match_ok!(result,true);
 }
 
 #[test]
 fn not_token_parser_test() {
     let result = not_parser("not".as_bytes());
-    assert_ok!(result,());
+    assert_full_match_ok!(result,());
 
     let result = not_parser("not rtert".as_bytes());
     assert_ok!(result,());
@@ -41,8 +61,30 @@ fn not_token_parser_test() {
 #[test]
 fn paren_token_parser_test() {
     let result = lparen_parser("(".as_bytes());
-    assert_ok!(result,());
+    assert_full_match_ok!(result,());
 
     let result = rparen_parser(")".as_bytes());
-    assert_ok!(result,());
+    assert_full_match_ok!(result,());
+}
+
+#[test]
+fn comment_token_parser_test() {
+    let result = comment_begin_parser("(*".as_bytes());
+    assert_full_match_ok!(result,());
+
+    let result = comment_end_parser("*)".as_bytes());
+    assert_full_match_ok!(result,());
+
+    let result = comment_parser("(**)".as_bytes());
+    assert_full_match_ok!(result,());
+
+    let result = comment_parser("(*aaa iii ううう*)".as_bytes());
+    assert_full_match_ok!(result,());
+
+    let result = comment_parser("(*aaa iii う)うう**)".as_bytes());
+    assert_full_match_ok!(result,());
+
+    let result = comment_parser("(*aaa iii(* aa *) う)うう**)".as_bytes());
+    assert_full_match_ok!(result,());
+
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -11,3 +11,18 @@ macro_rules! assert_ok {
        }
    }
 }
+
+#[cfg(test)]
+macro_rules! assert_full_match_ok {
+   ($r: expr, $tk: expr) => {
+       match &$r {
+           Ok((rem, item)) => {
+               assert_eq!(rem.len(),0);
+               assert_eq!(*item, $tk, "Remaining: {:?}", rem);
+           }
+           Err(_) => {
+               panic!("{:?}", $r);
+           }
+       }
+   }
+}
