a858e2bc146a1aa9447c615394bceb0b077267b8
elipmoc <relipmoc101@gmail.com>
2019-07-13 22:04:03 +0900

    [docs]opパーサのコメント追加

--- src/token_parser/op.rs
+++ src/token_parser/op.rs
@@ -7,15 +7,27 @@ define_ignore_str_parser!(minus_parser,"-");
 /// +演算子のパーサ
 define_ignore_str_parser!(plus_parser,"+");
 
-// その他よくわからんパーサ
+/// 浮動小数点数用の加算演算子
 define_ignore_str_parser!(plus_dot_parser,"+.");
+
+/// 浮動小数点数用の掛け算演算子
 define_ignore_str_parser!(ast_dot_parser,"*.");
+
+/// 浮動小数点数用の割り算演算子
 define_ignore_str_parser!(slash_dot_parser,"/.");
+
+/// 代入演算子
 define_ignore_str_parser!(equal_parser,"=");
+
+/// 大小比較演算子
 define_ignore_str_parser!(less_greater_parser,"<>");
+/// 大小比較演算子
 define_ignore_str_parser!(less_equal_parser,"<=");
+/// 大小比較演算子
 define_ignore_str_parser!(greater_equal_parser,">=");
+/// 大小比較演算子
 define_ignore_str_parser!(less_parser,"<");
+/// 大小比較演算子
 define_ignore_str_parser!(greater_parser,">");
 
 #[test]
