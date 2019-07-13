b559aa0be12ec5a428bfdc257e8bd745b1c19d94
elipmoc <relipmoc101@gmail.com>
2019-07-14 00:26:33 +0900

    [test]assrt_okマクロの修正

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -17,8 +17,8 @@ macro_rules! assert_full_match_ok {
    ($r: expr, $tk: expr) => {
        match &$r {
            Ok((rem, item)) => {
-               assert_eq!(rem.len(),0);
                assert_eq!(*item, $tk, "Remaining: {:?}", rem);
+               assert_eq!(rem.len(),0,"Remaining!");
            }
            Err(_) => {
                panic!("{:?}", $r);
